c662768171f97254ace337a7b83d8050d1815a54
Katherine Chau <katherine.chau@oehha.ca.gov>
2024-06-27 12:46:20 -0700

    ios fix

--- css/addons/unraid/login-page/matrix/js/matrix-code.js
+++ css/addons/unraid/login-page/matrix/js/matrix-code.js
@@ -2,6 +2,6 @@
 // Source: https://www.youtube.com/watch?v=ehvduomE0AU
 
 let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/michaelgermino.github.io\/css\/addons\/unraid\/login-page\/matrix\/video\/matrix-code.mp4" type="video\/mp4">Video not supported</video></div>
+<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted" playsinline> <source src="https:\/\/michaelgermino.github.io\/css\/addons\/unraid\/login-page\/matrix\/video\/matrix-code.mp4" type="video\/mp4">Video not supported</video></div>
 `
 document.body.innerHTML += video
